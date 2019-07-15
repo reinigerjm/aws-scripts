50dcf186a36c5e8f8985ce7ffbbd8f71cf0b454e
josh.reiniger <josh.reiniger@redfrogevents.com>
2019-07-15 15:42:24 -0500

    Image and snapshot cleanup for www-server images

---
+++ aws-image-cleanup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Find and deregister any www-server or eventsprout AMIs older than 3 days
+
+#Get some date variables today's date minus 3 days
+DATE=`date +%Y-%m-%d`
+DATEMINUS3=$(date -d "$DATE - 3 days" +%Y-%m-%d) 
+
+#Get the ImageIds for all www-server production images more than 3 days old
+declare -a TBD=(`aws ec2 describe-images --filters "Name=tag:Application,Values=www-server" "Name=tag:Environment,Values=production" --query "Images[*].[ImageId,CreationDate]" --output=json | jq -r '.[] | select (.[1]< "'$DATEMINUS3' | gmtime | todate") | .[0]'`)
+
+
+echo "Found "${#TBD[@]}" www-server images older than 3 days"
+
+#And then delete them along with their snapshots
+for i in "${TBD[@]}"
+do 
+	echo "Deleting image" $i
+	aws ec2 deregister-image --image-id $i --dry-run
+	SNAPSHOT=`aws ec2 describe-snapshots --filters "Name=description,Values=*"$i"*" --query 'Snapshots[*].SnapshotId' --output text`
+	echo "Deleting snapshot" $SNAPSHOT
+	aws ec2 delete-snapshot --snapshot-id $SNAPSHOT --dry-run
+done
